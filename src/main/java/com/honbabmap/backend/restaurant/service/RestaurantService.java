3c14bf0c1c7a8863425127bd2610fb01a6d09e96
lojhbm <lojhbm@gmail.com>
2026-06-08 12:09:28 +0900

    0608 service 메소드 구현 시작

--- src/main/java/com/honbabmap/backend/restaurant/service/RestaurantService.java
+++ src/main/java/com/honbabmap/backend/restaurant/service/RestaurantService.java
@@ -1,18 +1,19 @@
 package com.honbabmap.backend.restaurant.service;
 
-import com.honbabmap.backend.restaurant.repository.RestaurantRepository;
+import com.honbabmap.backend.restaurant.repository.*;
+import com.honbabmap.backend.review.repository.ReviewRepository;
+import com.honbabmap.backend.review.repository.ReviewTagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class RestaurantService {
+    // Repository
     private final RestaurantRepository restaurantRepository;
-
-    public RestaurantService(RestaurantRepository restaurantRepository) {
-        this.restaurantRepository = restaurantRepository;
-    }
-
+    private final StationRepository stationRepository;
+    private final ReviewRepository reviewRepository;
+    private final ReviewTagRepository reviewTagRepository;
 
 
 }
