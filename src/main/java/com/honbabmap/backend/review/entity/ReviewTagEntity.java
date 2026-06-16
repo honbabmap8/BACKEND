b59c4f15dbf0341f57570c4661660f1800ccf81d
dev-jaehyeok <chris030920@kookmin.ac.kr>
2026-06-16 13:51:06 +0900

    리뷰 레포지토리 생성

--- src/main/java/com/honbabmap/backend/review/entity/ReviewTagEntity.java
+++ src/main/java/com/honbabmap/backend/review/entity/ReviewTagEntity.java
@@ -9,14 +9,19 @@ import lombok.Setter;
 @Setter
 @Table(name = "reviews_tags")
 public class ReviewTagEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "review_tag_id", nullable = false)
     private Integer reviewTagId;
 
-    @Column(name = "review_id", nullable = false)
-    private Integer reviewId;
+    // 조인 추가: ReviewEntity와 다대일(N:1) 관계로 연결
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id", nullable = false)
+    private ReviewEntity review;
 
-    @Column(name = "tag_id", nullable = false)
-    private Integer tagId;
+    // 조인 추가: TagEntity와 다대일(N:1) 관계로 연결
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id", nullable = false)
+    private TagEntity tag;
 }
