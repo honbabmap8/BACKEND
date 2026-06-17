6decd5e1a1314b4f4e6138cab8930896caaf363e
dev-jaehyeok <chris030920@kookmin.ac.kr>
2026-06-17 15:49:09 +0900

    feat: 역 기준 식당 검색 API 페이징 추가

--- src/main/java/com/honbabmap/backend/restaurant/controller/RestaurantController.java
+++ src/main/java/com/honbabmap/backend/restaurant/controller/RestaurantController.java
@@ -5,8 +5,10 @@ import com.honbabmap.backend.restaurant.dto.RestaurantListResponse;
 import com.honbabmap.backend.restaurant.service.RestaurantService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Pageable;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,9 +20,14 @@ public class RestaurantController {
     // 역 기준 식당 검색
     @GetMapping("/station/{stationId}")
     public ResponseEntity<RestaurantListResponse> restaurantListByStation
-    (@PathVariable Integer stationId, @RequestParam Integer honbabLevel) {
+    (@PathVariable Integer stationId,
+     @PageableDefault(size = 5) Pageable pageable){ // 페이징 파라미터 추가
+
+        // 토큰 연동 전까지 테스트용 아이디 사용
+        String testLoginId = "jaehyeok";
+
         RestaurantListResponse response
-                = restaurantService.getRestaurantListByStation(stationId, honbabLevel);
+                = restaurantService.getRestaurantListByStation(stationId, testLoginId, pageable);
         return ResponseEntity.ok(response);
     }
 
