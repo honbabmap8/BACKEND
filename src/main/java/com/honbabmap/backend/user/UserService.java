6a3aac51f15d67992de775314312676a232b7448
lojhbm <lojhbm@gmail.com>
2026-06-18 15:04:50 +0900

    임시저장

--- src/main/java/com/honbabmap/backend/user/UserService.java
+++ src/main/java/com/honbabmap/backend/user/UserService.java
@@ -52,7 +52,7 @@ public class UserService {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
 
-        // JWT 토큰 생성 (현재는 임시 문자열)
+        // JWT 토큰 생성
         String accessToken = jwtTokenProvider.createToken(loginId);
 
         LoginResponse response = new LoginResponse("로그인에 성공했습니다.",
